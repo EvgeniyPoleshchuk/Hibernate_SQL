02f64556c219fed0b60bdff4c8f2ecfbd0b8100a
Евгений <2x2great@gmail.com>
2023-10-23 13:15:45 +0300

    changes

---
+++ src/main/resources/db/changelog/migrations/script.sql
@@ -0,0 +1,12 @@
+create table Persons
+(
+    id      serial,
+    name    varchar NOT NULL ,
+    surname varchar NOT NULL ,
+    age int NOT NULL ,
+    phone_number varchar NOT NULL ,
+    city_of_living varchar NOT NULL,
+    primary key (name,surname,age)
+);
+
+
